3f8ae30616c59ffda6cadd060846348d98eddf9b
Levi-Magny <lemmagny@gmail.com>
2021-09-09 19:02:17 -0300

    Finshing the course. The we implemented the theme feature with dark mode and light bright mode.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from './Components/GlobalStyle';
+import { BtnTema } from "./Components/UI";
+import SwitcherTemas from "./Components/SwitcherTemas";
+import { useState } from "react";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const handleTema = ()=>{
+    setTema(!tema);
+  };
+
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={handleTema}>
+         <SwitcherTemas tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
